4d22bba400d3681f20272667637bc8abac9ef7c1
xpf1991 <xpftsh@126.com>
2023-04-19 22:25:07 +0800

    finish part3-a curse

--- a/index.js
+++ a/index.js
@@ -1,6 +1,7 @@
-const { response } = require('express')
 const express = require('express')
 const app = express()
+
+app.use(express.json())
 
 let notes = [
     {
@@ -34,6 +35,7 @@ app.get('/api/notes', (request, response) => {
 app.get('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     const note = notes.find(note => note.id === id)
+    console.log(note, typeof (note))
     if (note) {
         response.json(note)
     } else {
@@ -41,6 +43,39 @@ app.get('/api/notes/:id', (request, response) => {
     }
 })
 
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(note => note.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        id: generateId(),
+        date: new Date(),
+        content: body.content,
+        importance: body.importance || false
+    }
+    //console.log(note)
+    notes = notes.concat(note)
+    response.json(notes)
+})
+
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
